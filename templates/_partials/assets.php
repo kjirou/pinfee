5aed3c78be2b8618e9c28e17ed8c546fb183391f
kjirou <kjirou.web@gmail.com>
2014-09-01 19:07:37 +0900

    Delete unnecessary files

--- templates/_partials/assets.php
+++
@@ -1,7 +0,0 @@
-<link href='http://fonts.googleapis.com/css?family=Fugaz+One|Fredoka+One' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="/css/base.css" type="text/css" />
-<script src="/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-<script src="/js/vendor/underscore-min.js"></script>
-<script src="/js/vendor/querystring.js"></script>
-<script src="/js/vendor/jquery-2.1.1.min.js"></script>
-<script src="/js/index.js"></script>
--- templates/_partials/header.php
+++
@@ -1,9 +0,0 @@
-<header class="header">
-  <h1><a href="/"><?= h($site_name) ?></a></h1>
-  <nav>
-    <ul>
-      <li><a href="/products/create.php" class="register">サービスを登録する</a></li>
-      <li><a href="javascript:void(0)" class="magnifier"><span class="hidden">(検索)</span></a></li>
-    </ul>
-  </nav>
-</header>
